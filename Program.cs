355809c450b336b572d5a8f336b12333ef719f2f
Sheetal <sheetalrana190@gmail.com>
2025-01-30 15:24:01 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+* Author: Sheetal Rana
+* Date: 29 January 2025
+* Project: Task_1
+* Description:
+* Calculate total money spent and average price of watching the game for Joe.
+*/
+
+namespace AssignmentTaskTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
