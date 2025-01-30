e8baada4c60eb45feba7993e80e53e91f1d4e503
Sheetal <sheetalrana190@gmail.com>
2025-01-30 16:20:52 -0500

    initializing variable to store number of tickets for different colour purchase and get the correct value from function GetValidNumber

--- Program.cs
+++ Program.cs
@@ -16,7 +16,36 @@ namespace AssignmentTaskTwo
             int PurpleTicketPrice = 50;
             int GreenTicketPrice = 80;
             int BlueTicketPrice = 100;
+            // initializing variable to store number of tickets for different colour purchase and get the correct value from function GetValidNumber
+            int PurpleTickets = GetValidNumber("Purple");
+            int GreenTickets = GetValidNumber("Green");
+            int BlueTickets = GetValidNumber("Blue");
 
+
+        }
+        // function to validate user input for number of tickets for different colour
+        static int GetValidNumber(string colour)
+        {
+            int NumOfTicketBuy = -1;
+            while (NumOfTicketBuy < 0)
+            {
+                try
+                {
+                    Console.WriteLine($"Enter number of trips to {colour}:");
+                    NumOfTicketBuy = int.Parse(Console.ReadLine());
+
+                    if (NumOfTicketBuy < 0)
+                    {
+                        Console.WriteLine("Number of trips cannot be negative. Please enter a non-negative number.");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid non-negative number.");
+                }
+            }
+
+            return trips;
         }
     }
 }
